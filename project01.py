c9cc517c0bbc1037997008f9ab9568759ef0504a
Vinícius Gomes <vsg.dev.tech@gmail.com>
2023-04-13 10:20:37 -0300

    Update project01.py

--- project01.py
+++ project01.py
@@ -1,38 +1,35 @@
-##--- Importação dos módulos ---##
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import cvzone
 import numpy as np
 
-##-- Definição das variáveis e iniciação do dispositivo de captura de imagem --##
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-detector = HandDetector(detectionCon=0.8) # Criação do detector e configuração da sua precisão
+detector = HandDetector(detectionCon=0.8) 
 colorR = (255, 0, 255)
 
 cx, cy, w, h = 50, 50, 100, 100
 
-##----------------------------------------##
 while True:
     success, img = cap.read()
-    img = cv2.flip(img, 1) # Inverter a imagem (Captura da tela) no eixo x
+    img = cv2.flip(img, 1) 
     img = detector.findHands(img)
     lmList, _ = detector.findPosition(img)
 
-    if lmList: # Encontrar a posição das falanges e Identificar a interação com os objetos
+    if lmList: 
 
         l, _, _ = detector.findDistance(8, 4, img)
         print(l)
         if l < 30:
             cursor = lmList[8]
-            if cx-w//2 < cursor[0] < cx+w//2 and cy-h//2 < cursor[1] < cy+h//2: # Se os pontos do Traking estiverem dentro das coordenadas que limitam a existência do quadrado, a cor do objeto mudará para verde
+            if cx-w//2 < cursor[0] < cx+w//2 and cy-h//2 < cursor[1] < cy+h//2: 
                 colorR = 0, 255, 0
                 cx, cy = cursor
 
             else:
                 colorR = (255, 0, 255)
 
-    cv2.rectangle(img, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), colorR, cv2.FILLED) # Criação do quadrado que aparecerá na tela
+    cv2.rectangle(img, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), colorR, cv2.FILLED) 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
